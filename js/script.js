525be9d208735567b5df12e048e0310a1aee7929
islam-fullstack <islam.qiyasov0823@gmail.com>
2021-07-30 20:13:36 +0500

    Style and API finish

---
+++ js/script.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const URL = 'http://hp-api.herokuapp.com/api/characters';
+
+    const searchInput = document.getElementById('searchBar');
+    const searchBtn = document.querySelector('.submit');
+    const contentInside = document.querySelector('.characterList');
+
+    getContent(URL);
+    async function getContent(url) {
+        try {
+            const resp = await fetch(url);
+            const data = await resp.json();
+            getDisplay(data);
+        }
+        catch (err) {
+            console.log(err)
+        }
+
+    }
+
+    function getDisplay(characters) {
+        console.log(characters);
+        characters.map((character) => {
+            const liELement = document.createElement('li');
+            liELement.innerHTML = `
+            <img src="${character.image}" alt="${character.name}">
+            <div class="character-info">
+                <h2 class="characterName">${character.name}</h2>
+                <p class="characterHouse">House: ${character.house}</p>
+            </div>
+            `;
+            contentInside.appendChild(liELement);
+        })
+    }
+
+
+});
